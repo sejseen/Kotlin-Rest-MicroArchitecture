658201cd8f4329d6dfbb897042b8ff7333280631
Konrad Tyma <Konrad.tyma@avra.com.pl>
2017-11-15 21:45:18 +0100

    Change structure for yml properties, add zulu proxy for springboot runner

--- api-gateway/src/main/kotlin/tyma/app/apigateway/ApiGatewayApplication.kt
+++ api-gateway/src/main/kotlin/tyma/app/apigateway/ApiGatewayApplication.kt
@@ -2,8 +2,10 @@ package tyma.app.apigateway
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @SpringBootApplication
+@EnableZuulProxy
 class ApiGatewayApplication
 
 fun main(args: Array<String>) {
